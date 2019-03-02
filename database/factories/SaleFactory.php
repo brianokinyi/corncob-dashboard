d4bc4c99d35bb8e765aabd43bc2d5d64691978a9
brianokinyi <brianokinyi.bo@gmail.com>
2019-03-02 12:49:49 +0300

    Add sales resources

---
+++ database/factories/SaleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Sale::class, function (Faker $faker) {
+    return [
+        'farmer_id' => function() {
+            return App\Models\Farmer::inRandomOrder()->first()->id;
+        },
+        'no_of_bags' => $faker->randomDigitNotNull,
+        'county' => $faker->randomElement([
+            'Narok',
+            'Nakuru',
+            'Trans Nzoia',
+            'Kitale'
+        ]),
+        'subcounty' => $faker->randomElement([
+            'narok',
+            'nakuru',
+            'Trans Nzoia',
+            'Kitale'
+        ]),
+        'price' => $faker->randomNumber(4)
+    ];
+});
